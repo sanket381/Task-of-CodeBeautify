1ae0c7c14eb1d45cf3082125f197c25e09187894
sanket381 <salunkesanket381@gmail.com>
2023-02-11 00:13:07 +0530

    first commit

--- logic.js
+++ logic.js
@@ -1,17 +1,21 @@
-$(function() {
-    $( "#imageListId" ).sortable({
-    update: function(event, ui) {
-        getIdsOfImages();
-    }//end update         
-    });
-});
-  
-function getIdsOfImages() {
-    var values = [];
-    $('.listitemClass').each(function (index) {
-        values.push($(this).attr("id")
-                .replace("imageNo", ""));
-    });
-      
-    $('#outputvalues').val(values);
-}
+function calculate() {
+
+
+    let totalHours = 0;
+    for (let i = 1; i <= 7; i++) {
+      const startTime = document.getElementById(`startTime${i}`).value;
+      const endTime = document.getElementById(`endTime${i}`).value;
+      const breakDuration = document.getElementById(`breakDuration${i}`).value;
+
+      const start = new Date("01/01/2021 " + startTime);
+      const end = new Date("01/01/2021 " + endTime);
+      const diff = end - start;
+      const diffInHours = diff / 1000/60/60 ;
+      console.log(diffInHours);
+      const total = diffInHours - breakDuration;
+      totalHours += total;
+      document.getElementById(`totalHours${i}`).value = total
+    }
+
+    document.getElementById("result").innerHTML = totalHours + " hours";
+  }
